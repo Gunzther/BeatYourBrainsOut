e0281b54dc21612ecb3c3a9bff8eef9de947b177
Gunzther <jenny.sirarat@gmail.com>
2020-11-01 00:14:34 +0700

    add test scene basic script

---
+++ Assets/Scripts/TestScene/TestButtonAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace BBO.BBO.TestScene
+{
+    public class TestButtonAction : MonoBehaviour
+    {
+#if UNITY_EDITOR
+
+        public void OnClickButton1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnClickButton2(bool val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnClickButton3(string val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnClickButton4(int val)
+        {
+            throw new NotImplementedException();
+        }
+
+#endif
+    }
+}
