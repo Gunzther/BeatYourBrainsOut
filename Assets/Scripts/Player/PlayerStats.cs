7b955f01257ff44303450063a413f5a90b05e3c9
Gunzther <jenny.sirarat@gmail.com>
2020-11-08 16:33:42 +0700

    create PlayerStats and PlayerCharacter

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace BBO.BBO.PlayerManagement
+{
+    public class PlayerStats
+    {
+        public int Health => PlayerPrefs.GetInt(key + playerID);
+
+        private readonly int playerID = default;
+        private const string key = "hp";
+        private const int defaultValue = 100;
+
+        public PlayerStats(int playerID)
+        {
+            this.playerID = playerID;
+            Clear();
+        }
+
+        public void Clear()
+        {
+            PlayerPrefs.SetInt(key + playerID, defaultValue);
+        }
+
+        public void SetPlayerHealth(int value)
+        {
+            PlayerPrefs.SetInt(key + playerID, value);
+        }
+
+        public void DecreasePlayerHealth(int value)
+        {
+            PlayerPrefs.SetInt(key + playerID, Health - value);
+        }
+
+        public void IncreasePlayerHealth(int value)
+        {
+            PlayerPrefs.SetInt(key + playerID, Health + value);
+        }
+    }
+}
