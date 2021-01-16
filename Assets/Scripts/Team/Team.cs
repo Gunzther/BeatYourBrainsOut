5363b5627115023564e1029259ffd27933286396
Gunzther <jenny.sirarat@gmail.com>
2021-01-16 10:32:22 +0700

    create Team

---
+++ Assets/Scripts/Team/Team.cs
@@ -0,0 +1,26 @@
+﻿using BBO.BBO.GameData;
+using BBO.BBO.PlayerManagement;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BBO.BBO.TeamManagement
+{
+    public class Team
+    {
+        public int PlayerAmount => playerCharacters.Count();
+        public int CurrentTeamHealth => playerCharacters.Select(x => x.CurrentPlayerStats.Health).Sum();
+        public int TotalHealth => PlayerAmount * PlayerData.DefaultHealth;
+
+        private HashSet<PlayerCharacter> playerCharacters = default;
+
+        public Team()
+        {
+            playerCharacters = new HashSet<PlayerCharacter>();
+        }
+
+        public void AddPlayer(PlayerCharacter playerCharacter)
+        {
+            playerCharacters.Add(playerCharacter);
+        }
+    }
+}
