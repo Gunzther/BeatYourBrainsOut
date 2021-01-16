15024f3449b95361fced88716d18ef8c8d582e26
Gunzther <jenny.sirarat@gmail.com>
2021-01-16 10:32:49 +0700

    create TeamManager

---
+++ Assets/Scripts/Team/TeamManager.cs
@@ -0,0 +1,29 @@
+﻿using BBO.BBO.PlayerManagement;
+using UnityEngine;
+
+namespace BBO.BBO.TeamManagement
+{
+    public class TeamManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform playerPrefab;
+
+        [SerializeField]
+        private Transform parent;
+
+        private Team team = default;
+
+        private void Start()
+        {
+            team = new Team();
+        }
+
+        public void SpawnNewPlayer(Vector3 position)
+        {
+            Transform newPlayer = Instantiate(playerPrefab, parent);
+            newPlayer.position = position;
+
+            team.AddPlayer(newPlayer.GetComponent<PlayerCharacter>());
+        }
+    }
+}
