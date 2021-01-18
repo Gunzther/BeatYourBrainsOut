5bb0feda428882e6eebb0baa299d1500e14730ff
Gunzther <jenny.sirarat@gmail.com>
2021-01-18 16:17:50 +0700

    implement UIManager

--- Assets/Scripts/Player/PlayerCharacter.cs
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using BBO.BBO.TeamManagement;
+using BBO.BBO.TeamManagement.UI;
+using UnityEngine;
 
 namespace BBO.BBO.PlayerManagement
 {
@@ -7,12 +9,25 @@ namespace BBO.BBO.PlayerManagement
         public PlayerStats CurrentPlayerStats { get; private set; }
 
         private int playerID = default;
+        private UIManager uiManager = default;
+        private Team team = default;
+
+        public void SetTeam(Team team)
+        {
+            this.team = team;
+        }
+
+        public void UpdateHpUI()
+        {
+            uiManager.SetTeamHpValue(team.CurrentTeamHealth);
+        }
 
         private void Awake()
         {
             // TODO: generate player id and assign value to playerID variable
             playerID = 0;
             CurrentPlayerStats = new PlayerStats(playerID);
+            uiManager = FindObjectOfType<UIManager>();
         }
     }
 }
