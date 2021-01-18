6355c7217360b2dc5ec703f75cf6d783f801f28d
Gunzther <jenny.sirarat@gmail.com>
2021-01-18 16:17:20 +0700

    create UIManager

---
+++ Assets/Scripts/Team/UI/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BBO.BBO.TeamManagement.UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider teamHpSlider;
+
+        [SerializeField]
+        private Text teamHpText;
+
+        public void SetTeamHpMaxValue(int value)
+        {
+            teamHpSlider.maxValue = value;
+            teamHpText.text = value.ToString();
+        }
+
+        public void SetTeamHpValue(int value)
+        {
+            teamHpSlider.value = value;
+            teamHpText.text = value.ToString();
+        }
+    }
+}
