b63ef0a1eab5f9bc96f87a44682b231178defd8b
Gunzther <jenny.sirarat@gmail.com>
2021-01-22 18:04:16 +0700

    create GameManager

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using BBO.BBO.TeamManagement;
+using BBO.BBO.Utilities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    [SerializeField]
+    TeamManager teamManager = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
