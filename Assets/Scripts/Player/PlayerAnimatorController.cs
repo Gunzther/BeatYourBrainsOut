d81d9d1b7c9a472e4652d6871df84d2d63015396
ppv_srun <1neungfirst@gmail.com>
2021-01-23 02:04:15 +0700

    finished baseball anim

---
+++ Assets/Scripts/Player/PlayerAnimatorController.cs
@@ -0,0 +1,24 @@
+﻿using BBO.BBO.TeamManagement;
+using BBO.BBO.TeamManagement.UI;
+using UnityEngine;
+
+namespace BBO.BBO.PlayerManagement
+{
+    public class PlayerAnimatorController : MonoBehaviour
+    {
+        [SerializeField]
+        private Texture[] spriteList;
+        [SerializeField]
+        private int spriteNumber = 0;
+
+        private Renderer renderer;
+        void Awake()
+        {
+            renderer = GetComponent<Renderer>();
+        }
+        void Update()
+        {
+            renderer.material.SetTexture("_MainTex", spriteList[spriteNumber < spriteList.Length - 1 ? spriteNumber : spriteList.Length - 1]);
+        }
+    }
+}
