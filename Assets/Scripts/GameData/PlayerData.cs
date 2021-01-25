da7203e3da476119d19fdf07576e2d85ac7d469a
Gunzther <jenny.sirarat@gmail.com>
2021-01-25 21:56:37 +0700

    move animation data to AnimationTriggerData

--- Assets/Scripts/GameData/PlayerData.cs
+++ Assets/Scripts/GameData/PlayerData.cs
@@ -5,10 +5,5 @@ namespace BBO.BBO.GameData
     public static class PlayerData
     {
         public const int DefaultHealth = 100;
-
-        public static readonly int IdleTriggerHash = Animator.StringToHash("Idle");
-        public static readonly int WalkFrontTriggerHash = Animator.StringToHash("WalkFront");
-        public static readonly int WalkBackTriggerHash = Animator.StringToHash("WalkBack");
-        public static readonly int WalkSideTriggerHash = Animator.StringToHash("WalkSide");
     }
 }
