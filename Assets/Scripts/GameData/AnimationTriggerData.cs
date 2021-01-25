bfe760f1a5190d31b9bd2359ba4495cd4bb5cf43
Gunzther <jenny.sirarat@gmail.com>
2021-01-25 21:55:24 +0700

    create AnimationTriggerData

---
+++ Assets/Scripts/GameData/AnimationTriggerData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BBO.BBO.GameData
+{
+    public static class AnimationTriggerData
+    {
+        public static readonly int IdleTriggerHash = Animator.StringToHash("Idle");
+        public static readonly int WalkFrontTriggerHash = Animator.StringToHash("WalkFront");
+        public static readonly int WalkBackTriggerHash = Animator.StringToHash("WalkBack");
+        public static readonly int WalkSideTriggerHash = Animator.StringToHash("WalkSide");
+        public static readonly int DeadTriggerHash = Animator.StringToHash("Dead");
+        public static readonly int HurtTriggerHash = Animator.StringToHash("Hurt");
+    }
+}
