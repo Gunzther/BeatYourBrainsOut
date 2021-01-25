9b02de1c588aa5f073be7167fe5e2736461dceca
Gunzther <jenny.sirarat@gmail.com>
2021-01-25 17:00:12 +0700

    create BBOSceneManager

---
+++ Assets/Scripts/GameManager/BBOSceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace BBO.BBO.GameManager
+{
+    public static class BBOSceneManager
+    {
+        public static void LoadScene(int index)
+        {
+            SceneManager.LoadScene(index);
+        }
+
+        public static void LoadScene(string name)
+        {
+            SceneManager.LoadScene(name);
+        }
+
+        public static IEnumerator LoadSceneAsync(string name, Action finishAction)
+        {
+            var async = SceneManager.LoadSceneAsync(name);
+
+            while (!async.isDone)
+            {
+                yield return null;
+            }
+
+            finishAction?.Invoke();
+        }
+    }
+}
