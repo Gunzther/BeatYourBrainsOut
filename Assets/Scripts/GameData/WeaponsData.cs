4cc4da7f4f87d0ade0dbb1dde5988c5b0854342e
Gunzther <jenny.sirarat@gmail.com>
2021-04-13 13:01:14 +0700

    move weapon type enum to WaponsData

--- Assets/Scripts/GameData/WeaponsData.cs
+++ Assets/Scripts/GameData/WeaponsData.cs
@@ -4,10 +4,18 @@
     {
         public enum Type
         {
-            Normal,
-            IntervalDamage,
-            TimesDamage,
-            Protect
+            Normal, // weapon that attack object
+            IntervalDamage, // weapon that attack for a certain time, then disappear
+            LimitAttacksNumber, // weapon that has limit number of attacks
+            Protected // weapon for protection
+        }
+
+        public enum Weapon
+        {
+            baseballbat,
+            stickyband,
+            nailbox,
+            nail
         }
     }
 }
