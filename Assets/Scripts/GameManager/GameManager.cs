90decca8f2d029c7309df38cdfb4a8fa7d28a878
Smart <korawit.r@ku.th>
2021-04-24 02:55:16 +0700

    update GameManager

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,39 +1,44 @@
-﻿using System.Collections;
-using BBO.BBO.TeamManagement;
+﻿using BBO.BBO.TeamManagement;
 using BBO.BBO.Utilities;
-using UnityEngine;
+using System;
 using UnityEngine.InputSystem;
 
-namespace BBO.BBO.GameManager
+namespace BBO.BBO.GameManagement
 {
     public class GameManager : MonoSingleton<GameManager>
     {
-        [SerializeField]
-        private TeamManager teamManager = default;
-        
-        // [SerializeField]
-        // private Animator musicAnimator;
-        
+        private bool activeSelectMap = false;
+        public bool ActiveSelectMap
+        {
+            get => activeSelectMap;
+            set => activeSelectMap = value;
+        }
+
         private void Start()
         {
             InputDevice inputDevice = InputSystem.devices[0];
-            teamManager.SetupLocalMultiplayer(inputDevice);
+            TeamManager.Instance.SetupLocalMultiplayer(inputDevice);
         }
-        
+
         private void Update()
         {
-            teamManager.AddPlayerFromController();
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (activeSelectMap)
             {
-                // musicAnimator.SetTrigger("fadeOut");
-                StartCoroutine(BBOSceneManager.LoadSceneAsync("Gameplay",
-                    () =>
-                    {
-                        teamManager.Reload();
-                        StopAllCoroutines();
-                    }
-                    ));
+                TeamManager.Instance.AddPlayerFromController();
+                StartGameManager sgm = FindObjectOfType<StartGameManager>();
+                sgm?.SelectMap();
             }
+        }
+
+        public void LoadSceneCoroutine(string name, Action action)
+        {
+            StartCoroutine(BBOSceneManager.LoadSceneAsync(name,
+                () =>
+                {
+                    action?.Invoke();
+                    StopAllCoroutines();
+                }
+            ));
         }
     }
 }
