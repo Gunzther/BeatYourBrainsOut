7adb22661cd789a74123b6f4a7f36b156896ffef
Gunzther <jenny.sirarat@gmail.com>
2021-04-25 18:40:07 +0700

    comment unused methods

--- Assets/Scripts/Player/PlayerAnimatorController.cs
+++ Assets/Scripts/Player/PlayerAnimatorController.cs
@@ -30,23 +30,23 @@ namespace BBO.BBO.PlayerManagement
             return playerAnimator.GetCurrentAnimatorStateInfo(0).IsName(stateName);
         }
 
-        public void BeginAttackAction(int shortWeaponDamageValue)
-        {
-            if (shortWeaponDamageValue > 0)
-            {
-                //set damage value depands on weapon
-                weaponCollider.SetDamageValue(shortWeaponDamageValue);
-                weaponCollider.gameObject.SetActive(true);
-            }
-        }
+        //public void BeginAttackAction(int shortWeaponDamageValue)
+        //{
+        //    if (shortWeaponDamageValue > 0)
+        //    {
+        //        //set damage value depands on weapon
+        //        weaponCollider.SetDamageValue(shortWeaponDamageValue);
+        //        weaponCollider.gameObject.SetActive(true);
+        //    }
+        //}
 
-        public void EndAttackAction(int isShortWeapon)
-        {
-            if (isShortWeapon == 1)
-            {
-                weaponCollider.gameObject.SetActive(false);
-            }
-        }
+        //public void EndAttackAction(int isShortWeapon)
+        //{
+        //    if (isShortWeapon == 1)
+        //    {
+        //        weaponCollider.gameObject.SetActive(false);
+        //    }
+        //}
 
         private void Awake()
         {
